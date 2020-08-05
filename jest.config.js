e8855eb2d73152a382af07e984ff7452be00213f
qrxt <qrx357@gmail.com>
2020-08-05 23:06:44 +0300

    добавляет начало переноса на puppeteer

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
-module.exports = {
-  "preset": "ts-jest",
+const merge = require("merge")
+const ts_preset = require("ts-jest/jest-preset")
+const puppeteer_preset = require("jest-puppeteer/jest-preset")
+
+module.exports = merge.recursive(ts_preset, puppeteer_preset, {
   "moduleNameMapper": {
     "^@interfaces/(.*)$": "<rootDir>/src/range/ts/interfaces/$1",
     "^@type/(.*)$": "<rootDir>/src/range/ts/type/$1",
@@ -8,4 +11,4 @@ module.exports = {
     "^@elements/(.*)$": "<rootDir>/src/range/elements/$1",
     "^@utils/(.*)$": "<rootDir>/src/range/utils/$1"
   }
-};
+});
